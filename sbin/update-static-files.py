e23a8c64b5c1e1a14e5be22e2d1dbb4e66d29d9c
Jachin Rupe <jachin.rupe@gmail.com>
2016-02-22 18:13:37 -0600

    Updating more of the theme files.

--- sbin/update-static-files.py
+++ sbin/update-static-files.py
@@ -9,6 +9,7 @@ theme_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 node_modules_path = os.path.join(theme_path, 'node_modules')
 static_path = os.path.join(theme_path, 'static')
 static_js_path = os.path.join(static_path, 'js')
+static_css_path = os.path.join(static_path, 'css')
 
 if not os.path.isdir(node_modules_path):
     print("The node_modules directory isn't there you need to run npm first.")
@@ -29,4 +30,17 @@ mdl_js_path = os.path.join(node_modules_path, 'material-design-lite', 'dist')
 for f in mdl_js_files:
     src = os.path.join(mdl_js_path, f)
     dest = os.path.join(static_js_path, f)
+    shutil.copy(src, dest)
+
+mdl_css_files = [
+    'material-grid.css',
+    'material-grid.min.css',
+    'material.css',
+    'material.min.css',
+    'material.min.css.map'
+]
+mdl_css_path = os.path.join(node_modules_path, 'material-design-lite', 'dist')
+for f in mdl_css_files:
+    src = os.path.join(mdl_css_path, f)
+    dest = os.path.join(static_css_path, f)
     shutil.copy(src, dest)
